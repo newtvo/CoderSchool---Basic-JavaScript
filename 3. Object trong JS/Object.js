7f1bae5e847314d35d443e35d29ef55338615ebd
Newt Vo <nhutmvo1997@gmail.com>
2020-08-25 13:22:27 -0400

    Added Object and Array

---
+++ 3. Object trong JS/Object.js
@@ -0,0 +1,15 @@
+/**
+ * Sử dụng kiến thức đã học khai báo object person gồm các property name, age, gender. 
+ * Ví dụ:
+ *  var person = {
+ *    name: 'Pham Thinh',
+ *    age: 25,
+ *    gender: 'male'
+ *  };
+ */
+ 
+var person = {
+    name: 'Vo Nhut',
+    age: 23,
+    gender: 'male'
+};
---
+++ 4. Array trong JS/Array.js
@@ -0,0 +1,18 @@
+/*
+Khai báo một array "listAnimal" chứa các object mô tả danh sách các con vật.
+Mỗi con vật có 2 thuộc tính: name và age trong đó name có kiểu string, age có kiểu number.
+ * {name}: string;
+ * {age}: number;
+*/
+
+var ape = {
+    name: "Ut Em",
+    age: 1
+};
+
+var horse = {
+    name: "Me Thuy",
+    age: 55
+};
+
+var listAnimal = [ape, horse];

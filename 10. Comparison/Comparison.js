55e57e5681d8f7f4edbe327ba18f67ff190cefc5
Newt Vo <nhutmvo1997@gmail.com>
2020-08-25 14:09:59 -0400

    Added Comparison

---
+++ 10. Comparison/Comparison.js
@@ -0,0 +1,6 @@
+var a = [1, 2];
+var b = [1, 2];
+console.log(a === b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù giá trị của 2 array có vẻ giống nhau. Viết câu trả lời ở dưới đây.
+
+// Lý do có sự khác  biệt vì a,b  là  2  object khác nhau mac du co cung gia tri

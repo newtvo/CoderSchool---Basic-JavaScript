bc925c2fefe8b852f54144b1d63e0f2e3b16c30d
Newt Vo <nhutmvo1997@gmail.com>
2020-08-25 13:47:55 -0400

    Added Arithmetic Operators

---
+++ 5. Arithmetic Operators/Triangle2.js
@@ -0,0 +1,16 @@
+var width = 10;
+var height = 20;
+
+/**
+ * width: Chiều rộng của 1 hình chữ nhật
+ * height: Chiều dài của hình chữ nhật
+ * Yêu cầu: 
+ * - Khai báo biến s là diện tích của hình chữ nhật này.
+ * - Khai báo biến p là chu vi của hình chữ nhật này.
+ */
+ 
+// viết code ở đây.
+
+var s = width * height;
+
+var p = (width + height) * 2;
